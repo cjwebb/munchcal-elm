9f1fa6b13f50ba93045126f5c7a8186c732a04c6
cjwebb <colinjameswebb@hotmail.com>
2016-06-14 16:19:41 +0100

    refactor, to pass data into functions

--- Main.elm
+++ Main.elm
@@ -1,26 +1,42 @@
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (..)
+
+type alias Meal =
+  { date : String
+  , name : String
+  }
+
+initialMeals : List Meal
+initialMeals =
+  [{ date = "Mon", name = "Salmon Pasta" }
+  ,{ date = "Tue", name = "Bratwurst & Frozen Veg" }
+  ,{ date = "Wed", name = "Casserole" }
+  ,{ date = "Thu", name = "Faggots & Green Beans" }
+  ,{ date = "Fri", name = "Fish Pie" }
+  ,{ date = "Sat", name = "???" }
+  ,{ date = "Sun", name = "Roast Beef" }
+  ]
 
 main =
-  div [mainDivStyle] [
-    ul [ulStyle]
-     [ renderDay "Mon" ["Salmon Pasta"]
-     , renderDay "Tue" ["Bratwurst & Frozen Veg"]
-     , renderDay "Wed" ["Casserole"]
-     , renderDay "Thu" ["Faggots & Green Beans"]
-     , renderDay "Fri" ["Fish Pie"]
-     , renderDay "Sat" []
-     , renderDay "Sun" ["Roast Beef"]
-     ]
-  ]
+  div [mainDivStyle]
+    [ p [titleStyle] [ text "For the week beginning 13th June" ]
+    , ul [ulStyle] (List.map renderDay initialMeals)
+    ]
+
+titleStyle : Attribute ()
+titleStyle =
+  style
+    [("margin-left", "40px")
+    ,("font-size", "14px")
+    ]
 
 mainDivStyle : Attribute ()
 mainDivStyle =
   style
-    [("width","800px")
-    ,("margin","0 auto")
+    [("margin","0 auto")
     ,("margin-top","50px")
     ,("font-family","Helvetica Neue,Helvetica,Arial,sans-serif")
+    ,("color", "#333")
     ]
 
 ulStyle : Attribute ()
@@ -29,18 +45,12 @@ ulStyle =
     [("list-style-type", "none")
     ]
 
-renderDay : String -> List String -> Html msg
-renderDay name meals =
-  li [renderDayStyle] [
+renderDay : Meal -> Html msg
+renderDay meal =
+  li [] [
     div []
-      [ h2 [] [ text name ]
-      , p [] [ text (Maybe.withDefault "???" (List.head meals)) ]
+      [ h2 [] [ text meal.date ]
+      , p [] [ text meal.name ]
       ]
   ]
-
-renderDayStyle : Attribute msg
-renderDayStyle =
-  style
-    [("border-bottom","1px solid black")
-    ]
 
